686bebf410cea6a2bdf1a35db321c3ab41eac49a
Parth Patel <parth1042020@gmail.com>
2026-03-11 21:18:51 +0530

    Update movieService.js

--- movie-review-frontend/src/api/movieService.js
+++ movie-review-frontend/src/api/movieService.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://movie-review-backend-1r30.onrender.com";
+const BASE_URL = "https://movie-review-app-vado.onrender.com";
 
 export const getAllMovies = async () => {
   const res = await fetch(`${BASE_URL}/api/v1/movies`);
